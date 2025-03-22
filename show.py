5384a3559cfb1365da342c14d3791683b2b29a27
Hussein Alaa <106111576+hu55ain3laa@users.noreply.github.com>
2025-03-23 00:19:58 +0300

    remove

--- show.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load the dataset
-file_path = "./bank_users_dataset_encrypted.csv"
-file_path = "./bank_users_dataset_decrypted.csv"
-df = pd.read_csv(file_path)
-
-# Display the first 10 rows
-print(df.head(10))
